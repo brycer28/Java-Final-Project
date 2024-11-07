c292ee70003ab7e9787fa691b36b6ea2617baac8
brycer28 <brycer2825@gmail.com>
2024-11-06 19:33:19 -0600

    Gave Card class a imgPath instance variable

--- src/Logic/Card.java
+++ src/Logic/Card.java
@@ -3,6 +3,7 @@ package Logic;
 public class Card {
     private final Suit suit;
     private final Rank rank;
+    private final String imgPath;
     private boolean faceUp = false;
 
     public enum Suit {
@@ -18,8 +19,33 @@ public class Card {
     public Card(Suit suit, Rank rank) {
         this.suit = suit;
         this.rank = rank;
+        this.imgPath = getImgPath();
     }
 
+    public String getImgPath() {
+        char s = suit.name().toLowerCase().charAt(0);
+
+        String r=null;
+        if (rank == Rank.TWO) {r = "2";}
+        else if (rank == Rank.THREE) {r = "3";}
+        else if (rank == Rank.FOUR) {r = "4";}
+        else if (rank == Rank.FIVE) {r = "5";}
+        else if (rank == Rank.SIX) {r = "6";}
+        else if (rank == Rank.SEVEN) {r = "7";}
+        else if (rank == Rank.EIGHT) {r = "8";}
+        else if (rank == Rank.NINE) {r = "9";}
+        else if (rank == Rank.TEN) {r = "10";}
+        else if (rank == Rank.JACK) {r = "j";}
+        else if (rank == Rank.QUEEN) {r = "q";}
+        else if (rank == Rank.KING) {r = "k";}
+        else if (rank == Rank.ACE) {r = "a";}
+
+        try{
+            return (r+s+".png");
+        } catch (NullPointerException e){
+            return null;
+        }
+    }
     @Override
     public String toString() {
         return this.getRank().toString() + " of " + this.getSuit().toString();
