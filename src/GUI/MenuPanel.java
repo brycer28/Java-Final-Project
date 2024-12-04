95e9cef4f2c9ae03997ec50acb66e58451ad7342
MasonSimpson <158009210+MasonSimpson@users.noreply.github.com>
2024-12-03 19:02:19 -0600

    Added basics to MenuPanel and MainGraphics.java. No functionality yet.

--- src/GUI/MenuPanel.java
+++ src/GUI/MenuPanel.java
@@ -1,10 +1,59 @@
 package GUI;
 
 import javax.swing.*;
+import Logic.*;
+import java.awt.*;
 
 public class MenuPanel extends JPanel {
 
+    private JLabel gameTitle;
+    private JLabel gameSubtitle;
+    private JButton pokerButton;
+    private JButton solitaireButton;
+    private JButton blackjackButton;
+    private JButton game4Button;
+    //private TexasHoldEmPanel pokerPanel;
+    private SolitairePanel solitairePanel;
+    private BlackjackPanel blackjackPanel;
+    //private Game4Panel game4Panel;
+
+
     public MenuPanel() {
-        //display the menu to select game and eventually select theme
+        super();
+        this.setLayout(null);
+        this.setBackground(new Color(0,100,0));
+        this.setVisible(true);
+
+        // Game title
+        gameTitle = new JLabel("52");
+        gameTitle.setFont(new Font("Serif", Font.BOLD, 80));
+        gameTitle.setBounds(700, 50, 200, 200);
+        gameTitle.setForeground(Color.WHITE);
+        this.add(gameTitle);
+
+        // Game subtitle
+        gameSubtitle = new JLabel("Card Game Suite");
+        gameSubtitle.setFont(new Font("Serif", Font.BOLD, 40));
+        gameSubtitle.setBounds(585,120, 800, 200);
+        gameSubtitle.setForeground(Color.WHITE);
+        this.add(gameSubtitle);
+
     }
+
+    private void startPoker() {
+
+    }
+
+    private void startSolitaire() {
+
+    }
+
+    private void startBlackjack() {
+
+    }
+
+    private void startGame4() {
+
+    }
+
 }
--- src/Main/MainGraphics.java
+++ src/Main/MainGraphics.java
@@ -1,5 +1,20 @@
 package Main;
 
 
+import GUI.BlackjackPanel;
+import GUI.MenuPanel;
+import Logic.Blackjack;
+
+import javax.swing.*;
+
 public class MainGraphics {
+    public static void main(String[] args) {
+        MenuPanel menu = new MenuPanel();
+
+        JFrame frame = new JFrame("52 - Card Game Suite");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1500,1000);
+        frame.add(menu);
+        frame.setVisible(true);
+    }
 }
